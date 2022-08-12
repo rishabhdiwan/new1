712bea7c66b2b58c1a9ec2a044d48fcb95c42af8
rishabhdiwan <89631292+rishabhdiwan@users.noreply.github.com>
2022-08-12 16:49:17 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -36,3 +36,38 @@ exports.default = series(
   watchTask
 );
 
+
+
+
+//another approach toptal
+// const gulp = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const concat = require('gulp-concat');
+// const cleanCSS = require('gulp-clean-css'); 
+// gulp.task('pack-js', function () {    
+//     return gulp.src(['js/**/*.js'])
+//         .pipe(cleanCSS())
+//         .pipe(concat('bundle.js'))
+//         .pipe(gulp.dest('build/js'))
+// });
+ 
+// gulp.task('pack-css', function () {    
+//     return gulp.src(['scss/**/*.scss'])
+//         .pipe(sass())
+//         .pipe(concat('stylesheet.css'))
+//         .pipe(gulp.dest('build/css'))
+// });
+// gulp.task('pack-css11', function () {    
+//     return gulp.src(['scss/**/*.scss'])
+//         .pipe(sass()) 
+//         .pipe(cleanCSS())
+//         .pipe(concat('stylesheet2.css'))
+//         .pipe(gulp.dest('build/css'))
+// });
+// gulp.task('watch', function() {
+//     gulp.watch('js/**/*.js', ['pack-js']);
+//     gulp.watch('scss/**/*.scss', ['pack-css']);
+//     gulp.watch('scss/**/*.scss', ['pack-css11']);
+//    });
+//    gulp.task('default', gulp.parallel('pack-js', 'pack-css', 'pack-css11')
+//    );
